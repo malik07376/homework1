df8d721eddc084caaf773c27368dd5f19771b34f
malik07376 <kudretillaevabdumalik@gmail.com>
2025-03-15 20:21:47 +0600

    first update

---
+++ mysite/source/models.py
@@ -0,0 +1,23 @@
+import django
+from django.db import models
+from django.contrib import admin
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import path
+# Create your models here.
+
+class Director(models.Model):
+    director_name=models.CharField(max_length=30)
+    age=models.IntegerField()
+    is_active=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.director_name
+class Actor(models.Model):
+    actor_name=models.CharField(max_length=30)
+    age=models.IntegerField()
+    is_active=models.BooleanField(default=False)
+    # actor_image=models.ImageField(upload_to='#')
+
+    def __str__(self):
+        return self.actor_name
